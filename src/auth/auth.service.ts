c5f5cdcef4ad75fdf26e2258ecda781d31269478
adjapic97 <djapic.andrej@gmail.com>
2022-08-14 14:33:12 +0200

    feature/prisma-implementation - added prisma, generated user model

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,11 +1,15 @@
 import { Injectable } from "@nestjs/common";
+import { PrismaService } from "src/prisma/prisma.service";
+import { AuthDto } from "./dto";
 
 
 @Injectable()
 export class AuthService {
 
-    signup() {
+    constructor(private prismaService: PrismaService) { }
 
+    signup(dto: AuthDto) {
+        console.log(dto);
     }
 
     signin() {
